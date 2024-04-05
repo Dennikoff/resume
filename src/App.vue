9d9260487d47c88b3b8e4e378ea5665c96e553bc
dharke <d.harke@yandex.ru>
2024-04-06 02:21:09 +0300

    make lienar gradient

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navigation from '@/components/Navigation.vue'
+import MainBanner from '@/components/MainBanner.vue'
 
 </script>
 
 <template>
   <div>
 		<Navigation/>
+		<MainBanner/>
 	</div>
 </template>
 
---
+++ src/components/MainBanner.vue
@@ -0,0 +1,56 @@
+<template>
+	<div id="main-banner" class="flex"> 
+		<div class="mx-auto title-container">
+			<h1 class="main-title orange-gradient">Front-end</h1>
+			<h2 class="main-subtitle">Developer</h2>
+		</div>
+		
+		
+	</div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/variables';
+.main-title {
+	text-align: center;
+	text-transform: uppercase;
+	font-size: 12rem;
+	font-weight: 900;
+	line-height: 1;
+	margin-inline: auto;
+}
+
+.title-container {
+	position: relative;
+	display: flex;
+}
+
+.main-subtitle {
+	position: absolute;
+	top: 100%;
+	left: -5%;
+	text-align: center;
+	text-transform: uppercase;
+	font-size: 10rem;
+	line-height: 1;
+	font-weight: 900;
+	background: conic-gradient(from 255deg at 50% 60%, $gradien-start, $gradien-end);
+  -webkit-background-clip: text;
+  -webkit-text-stroke: 10px transparent;
+  color: $dark;
+
+  // -webkit-text-stroke-color: conic-gradient(from 255deg at 50% 60%, $gradien-start, $gradien-end);
+}
+
+.orange-gradient {
+  background-image: conic-gradient(from 255deg at 50% 60%, $gradien-start, $gradien-end);
+  color: transparent;
+  background-clip: text;
+  -webkit-background-clip: text;
+}
+</style>
